fa778b0744b427811e9df81cccd26e38f2bca2c3
Splotchy <Brabbi67@gmail.com>
2026-04-19 08:18:01 +0300

    Basic Snake Clone

---
+++ Snake.py
@@ -0,0 +1,88 @@
+import pygame
+import random
+
+pygame.init()
+
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Sanke")
+running = True
+
+WIDTH, HEIGHT = 600, 400
+Border = pygame.Rect(0, 0, WIDTH, HEIGHT)
+Clock = pygame.time.Clock()
+x = 300
+y = 200
+Base_speed = 1
+Segments = [(300,200)]
+Direction = (1,0)
+Barrier = pygame.Rect(0,0,600,400)
+
+Move_delay = 150 #ms
+Last_move_time = pygame.time.get_ticks()
+
+food = (random.randint(0,29) * 20, random.randint(0,19) * 20)
+
+Game_over = False
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if not Game_over:
+        new_direction = Direction       
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            new_direction = (-1, 0)
+        if keys[pygame.K_d]:
+            new_direction = (1, 0)
+        if keys[pygame.K_w]:
+            new_direction = (0, -1)
+        if keys[pygame.K_s]:
+            new_direction = (0, 1)
+
+        dx, dy = Direction
+        if new_direction !=(-dx,-dy):
+            Direction = new_direction
+
+        current_time = pygame.time.get_ticks()
+        if current_time - Last_move_time >= Move_delay:
+            head_x, head_y = Segments[0]
+            dx, dy = Direction
+            new_head = (head_x + dx * 20, head_y + dy * 20)
+            if new_head in Segments:
+                Game_over = True
+                print("Game Over","\nScore:",len(Segments))
+            Segments.insert(0, new_head)
+            if new_head == food:
+                food = (random.randint(0, 29) * 20, random.randint(0, 19) * 20)
+            else:
+                Segments.pop()
+            head_x, head_y = Segments[0]
+            if head_x < 0 or head_x >= WIDTH or head_y < 0 or head_y >= HEIGHT:
+                Game_over = True
+                print("Game Over","\nScore:",len(Segments))
+
+            Last_move_time = current_time
+
+        screen.fill((0,0,0))
+
+        pygame.draw.rect(screen, (255,255,255), Border, 2)
+
+        for part in Segments:
+            pygame.draw.rect(screen, (0,255,0), (*part,20,20))
+        
+        pygame.draw.rect(screen,(255,0,0), (*food, 20, 20))
+    else:
+        if Game_over == True:
+            pygame.font.init()
+            font = pygame.font.SysFont("Arial", 30, bold=True)
+            text = font.render("Game Over", True,(0,0,0), (255,255,255))
+            screen.blit(text,(220,160))
+
+    pygame.display.update()
+    Clock.tick(60)
+
+
+pygame.quit()
